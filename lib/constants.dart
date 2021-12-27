73950bce71138fe5d8675921d7c076aea649096a
sumanth <sumanth.badugu07@gmail.com>
2021-12-27 22:15:11 +0530

    updated

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color login_bg = Color(0XDD000000);
+const Color signup_bg = Color(0xB3FFFFFF);
+
+const double defpaultPadding = 16.0;
+const Duration defaultDuration = Duration(milliseconds: 300);
